6bcf1f900567484c96d01bfbce30486f7cb99970
Pascal Knecht <pascal.knecht99@gmail.com>
2025-03-13 23:13:54 +0100

    Changed header

--- apps/nextjs/src/app/(marketing)/header.tsx
+++ apps/nextjs/src/app/(marketing)/header.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import FeedbackButton from "./feedback";
 import { Links } from "./links";
-import Image from "next/image";
 
 export async function Header() {
   return (
@@ -11,14 +10,7 @@ export async function Header() {
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex gap-8 items-center">
           <Link href="/" className="flex gap-2 items-center text-xl">
-            <Image
-              className="rounded w-8 h-8"
-              width="50"
-              height="50"
-              src="/computer.jpeg"
-              alt="hero image"
-            />
-            <span className="font-bold">StarterKit</span>
+            <span className="font-bold">Starter Kit</span>
           </Link>
 
           <Links />
